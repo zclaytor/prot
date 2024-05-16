c1dd15fb24168d50c36bd7e497c1fe1804e9bcb7
Zach Claytor <zclaytor@stsci.edu>
2024-05-16 13:42:20 -0400

    source MPLSTYLE directly from lightkurve

--- prot/__init__.py
+++ prot/__init__.py
@@ -1,7 +1,4 @@
-import os
-
-PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
-MPLSTYLE = "{}/misc/lightkurve.mplstyle".format(PACKAGEDIR)
+from lightkurve import MPLSTYLE
 
 from .wavelet_transform import WaveletTransform
 
