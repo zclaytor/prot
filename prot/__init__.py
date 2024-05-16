16f2546e3364865ecc5fb061806d5f5beca4d251
Zach Claytor <zclaytor@stsci.edu>
2024-05-16 13:28:04 -0400

    move version number to __init__.py

--- prot/__init__.py
+++ prot/__init__.py
@@ -3,5 +3,6 @@ import os
 PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
 MPLSTYLE = "{}/misc/lightkurve.mplstyle".format(PACKAGEDIR)
 
-from .version import __version__
 from .wavelet_transform import WaveletTransform
+
+__version__ = "0.2.0"
--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
 import setuptools
+import codecs
+import os.path
 
-# Load the __version__ variable without importing the package already
-exec(open("prot/version.py").read())
+
+# These two functions are just to read the version number. Sourced from
+# https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 setuptools.setup(
     name="prot",
-    version=__version__,
+    version=get_version("prot/__init__.py"),
     author="Zachary R. Claytor",
     author_email="zclaytor@hawaii.edu",
     install_requires=["lightkurve"],
